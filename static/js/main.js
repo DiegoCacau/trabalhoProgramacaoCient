21c8ba3e09e50c5e456edd3e98f3b3a4512fdfcc
Diego <dc_cacau@hotmail.com>
2017-12-13 17:56:04 -0200

    arquios iniciais

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+// runge kutta de 1 a 4
+// 1 de passo multiplo, inicializado por runge kutta de 1 ordem
+// 1 implicito
+
+jQuery(document).ready(function() {
+	var massa = 0.11;
+	var v0 = 8;
+	
+
+
+});
